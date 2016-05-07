1ebe9e2040d03df257780972bed68a086f49230f
Christopher Henry <chenry@mcs.anl.gov>
2016-05-07 01:22:13 -0500

    Loading tested fixes in genome annotation script in CI into staging

--- scripts/njs-run-genome-annotation.pl
+++ scripts/njs-run-genome-annotation.pl
@@ -220,6 +220,7 @@ if (defined($parameters->{find_close_neighbors}) && $parameters->{find_close_nei
 if (defined($parameters->{call_features_prophage_phispy}) && $parameters->{call_features_prophage_phispy} == 1)  {
 	push(@{$workflow->{stages}},{name => "call_features_prophage_phispy"});
 }
+push(@{$workflow->{stages}},{name => "renumber-features"});
 my $genome = $gaserv->run_pipeline($inputgenome, $workflow);
 $genome->{gc_content} = 0.5;
 if(defined($inputgenome->{gc_content})){
@@ -285,6 +286,11 @@ if (defined($genome->{features})) {
 			$ftr->{location}->[0]->[3] = $ftr->{location}->[0]->[3]+0;
 		}
 		delete $ftr->{feature_creation_event};
+	}
+}
+if (defined($genome->{close_genomes})) {
+	for (my $i=0; $i < @{$genome->{close_genomes}}; $i++) {
+		$genome->{close_genomes}->[$i]->{closeness_measure} = $genome->{close_genomes}->[$i]->{closeness_measure} + 0;
 	}
 }
 delete $genome->{contigs};
